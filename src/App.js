c848d1f2d8b810bc9670966e73bed24fb9ac2759
Saylendra Yasin <saylendrayasin@gmail.com>
2022-11-22 20:28:41 +0800

    Add Bot

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 import { Routes, Route } from "react-router-dom";
-import Home from './pages/home';
-import About from './pages/about';
-import Details from './pages/details';
-import FoodDetails from './components/fooddetails';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Details from "./pages/details";
+import FoodDetails from "./components/fooddetails";
+import Chatbot from "./pages/chatbot";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/chatbot" element={<Chatbot />} />
       <Route path="/details" element={<Details />}>
         <Route path=":foodId" element={<FoodDetails />} />
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
--- src/components/food.js
+++ src/components/food.js
@@ -1,41 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import FoodItems from '../components/fooditem';
+import React, { useState, useEffect } from "react";
+import FoodItems from "../components/fooditem";
 
 const Food = () => {
-    const [foods,setFood] = useState([]);
-    const [text, setText] = useState('');
+  const [foods, setFood] = useState([]);
+  const [text, setText] = useState("");
 
-    useEffect(() => {
-        fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=f20536e2a9f8460ba2ac66356e6fa945')
-        .then(response => response.json())
-        .then(data => setFood(data.results));
+  useEffect(() => {
+    fetch(
+      "https://api.spoonacular.com/recipes/complexSearch?apiKey=f20536e2a9f8460ba2ac66356e6fa945"
+    )
+      .then((response) => response.json())
+      .then((data) => setFood(data.results));
 
-        //eslint-disable-next-line
-    }, []);
+    //eslint-disable-next-line
+  }, []);
 
-    const handleChange = e => setText(e.target.value);
-    const handleClick = (e) => {
-        e.preventDefault();
-        fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${text}&number=&apiKey=f20536e2a9f8460ba2ac66356e6fa945`)
-        .then(response => response.json())
-        .then(data => setFood(data.results));
-        
-        setText('');
-    }
+  const handleChange = (e) => setText(e.target.value);
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?query=${text}&number=&apiKey=f20536e2a9f8460ba2ac66356e6fa945`
+    )
+      .then((response) => response.json())
+      .then((data) => setFood(data.results));
 
-    return (
-        <section className="py-5">
-            <div className="container px-4 px-lg-5 mt-5">
-                <div className="input-group mb-3" >
-                    <input type="text" className="form-control" placeholder="Find your meal..." aria-label="Find your meal.." aria-describedby="button-addon2" onChange={handleChange} value={text}/>
-                    <button className="btn btn-secondary" type="button" id="button-addon2" onClick={handleClick}>Search</button>
-                </div>
-                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    {foods.map((food)=> <FoodItems key={food.id} food={food} />) }
-                </div>
-            </div>
-        </section>
-    );
-}
+    setText("");
+  };
+
+  return (
+    <section className="py-5">
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Find your meal..."
+            aria-label="Find your meal.."
+            aria-describedby="button-addon2"
+            onChange={handleChange}
+            value={text}
+          />
+          <button
+            className="btn btn-secondary"
+            type="button"
+            id="button-addon2"
+            onClick={handleClick}
+          >
+            Search
+          </button>
+        </div>
+        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+          {foods.map((food) => (
+            <FoodItems key={food.id} food={food} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Food;
